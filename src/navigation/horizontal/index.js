f924560c015a3696a5ca3112be51e39d41a79f82
vosquery <vosquery@gmail.com>
2021-07-27 11:24:56 +0400

    transfer to /

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,22 @@
+// import { Mail, Home } from 'react-feather'
+
+// export default [
+//   {
+//     id: 'home',
+//     title: 'Home',
+//     icon: <Home size={20} />,
+//     navLink: '/home'
+//   },
+//   {
+//     id: 'sales',
+//     title: 'Sales Management',
+//     icon: <Mail size={20} />,
+//     navLink: '/sales-management'
+//   },
+//   {
+//     id: 'gigs',
+//     title: 'Gigs Management',
+//     icon: <Mail size={20} />,
+//     navLink: '/gigs-management'
+//   }
+// ]
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,19 @@
+// ** Redux Imports
+import { combineReducers } from 'redux'
+
+// ** Reducers Imports
+import auth from './auth'
+import navbar from './navbar'
+import layout from './layout'
+import ecommerce from './ecommerce'
+import invoice from './invoice'
+
+const rootReducer = combineReducers({
+  auth,
+  navbar,
+  layout,
+  ecommerce,
+  invoice
+})
+
+export default rootReducer
