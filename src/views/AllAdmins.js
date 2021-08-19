970f0b0e87c7abbd05cac2950563676ef5a20dc8
vosquery <vosquery@gmail.com>
2021-08-19 14:48:28 +0400

    all admins list

--- src/views/AllAdmins.js
+++ src/views/AllAdmins.js
@@ -1,28 +1,39 @@
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'reactstrap'
 
 const AllAdmins = () => {
-    const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
+  
+  async function readAdmins() {
+    const ref = await axios.get(`${process.env.REACT_APP_BASE_URL}/admin/`)
+    const data = await ref.data
+    if (!data) return []
+    return data
+  }
     
-    async function readAdmins() {
-      const ref = await axios.get(`${process.env.REACT_APP_BASE_URL}/admin/`)
-      const data = await ref.data
-      if (!data) return []
-      return data
+  async function fetchData() {
+    const res = await readAdmins()
+    if (res) {
+      setUsers(res)
     }
+  }
     
-    useEffect(() => {
-        async function fetchData() {
-          const res = await readAdmins()
-          if (res) {
-            setUsers(res)
-          }
-        }
-        fetchData()
-    }, [])
+  const deleteAdmin = useCallback(async (id) => {
+    alert(`Are you sure you want to delete ${id}?`)
+    const res = await axios.delete(`${process.env.REACT_APP_BASE_URL}/admin/${id}`)
+    if (res.status === 200) {
+      alert('User was deleted successfully!')
+      fetchData()
+    }
+  }, [])
     
+  useEffect(() => {
+    console.log('render')
+    fetchData()
+  }, [deleteAdmin])
+  
   return (
     <div>
       <Card>
@@ -30,7 +41,6 @@ const AllAdmins = () => {
           <CardTitle>List of admin users</CardTitle>
         </CardHeader>
         <CardBody>
-          <CardText>
             {users?.map((user, index) => {
               return (
                 <div key={user.email} style={{display: 'flex', justifyContent: 'space-between'}}>
@@ -38,16 +48,14 @@ const AllAdmins = () => {
                     <span> {index + 1}. </span>
                     <span><strong>Name:</strong> {user.name} </span>
                   </div>
-
                   <span><strong>Email:</strong> {user.email} </span>
                   <span><strong>Status:</strong> {user.status} </span>
-                  <span>
+                  <span style={{color: 'red'}} onClick={() => deleteAdmin(user._id)}>
                     delete
                   </span>
                 </div>
               )
             })}
-          </CardText>
           <CardText>
 
           </CardText>
