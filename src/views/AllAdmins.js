0041a3b7cf433765b8e6a83e6ea6787b0e76a77f
vosquery <vosquery@gmail.com>
2021-08-20 16:45:04 +0400

    all users list

--- src/views/AllAdmins.js
+++ src/views/AllAdmins.js
@@ -5,6 +5,8 @@ import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'react
 
 const AllAdmins = () => {
   const [users, setUsers] = useState([])
+  const [modal, setModal] = useState(false)
+  const [deletedUser, setDeletedUser] = useState('')
   
   async function readAdmins() {
     const ref = await axios.get(`${process.env.REACT_APP_BASE_URL}/admin/`)
@@ -21,12 +23,12 @@ const AllAdmins = () => {
   }
     
   const deleteAdmin = useCallback(async (id) => {
-    alert(`Are you sure you want to delete ${id}?`)
     const res = await axios.delete(`${process.env.REACT_APP_BASE_URL}/admin/${id}`)
     if (res.status === 200) {
       alert('User was deleted successfully!')
       fetchData()
     }
+    setModal(false)
   }, [])
     
   useEffect(() => {
@@ -34,8 +36,26 @@ const AllAdmins = () => {
     fetchData()
   }, [deleteAdmin])
   
+  const openModal = (id) => {
+    setDeletedUser(id)
+    setModal(true)
+  }
+  
+  const Modal = () => {
+    return (
+      <div style={{position: 'fixed', top: '30%', left: '50%', width: '30%', textAlign: 'center', padding: 50, backgroundColor: '#FFFFFF', transform: 'translate(-50%, -50%)', zIndex: '10'}}>
+        <h4 style={{margin: '1rem 0'}}>Are you sure you want to delete this User?</h4>
+        <div style={{display: 'flex', justifyContent: 'space-between', width: '60%', margin: '0 auto'}}>
+          <button onClick={() => setModal(false)}>cancel</button>
+          <button onClick={() => deleteAdmin(deletedUser)}>yes</button>
+        </div>
+      </div>
+    )
+  }
+  
   return (
     <div>
+      {modal && <Modal />}
       <Card>
         <CardHeader>
           <CardTitle>List of admin users</CardTitle>
@@ -43,15 +63,16 @@ const AllAdmins = () => {
         <CardBody>
             {users?.map((user, index) => {
               return (
-                <div key={user.email} style={{display: 'flex', justifyContent: 'space-between'}}>
-                  <div>
+                <div key={user.email} style={{display: 'flex', justifyContent: 'space-between', height: 30}}>
+                  <div style={{width: '25%'}}>
                     <span> {index + 1}. </span>
                     <span><strong>Name:</strong> {user.name} </span>
                   </div>
-                  <span><strong>Email:</strong> {user.email} </span>
-                  <span><strong>Status:</strong> {user.status} </span>
-                  <span style={{color: 'red'}} onClick={() => deleteAdmin(user._id)}>
-                    delete
+                  <span style={{width: '20%'}}><strong>Email:</strong> {user.email} </span>
+                  <span style={{width: '20%'}}><strong>Status:</strong> {user.status} </span>
+                  {/* <span style={{color: 'red'}} onClick={() => deleteAdmin(user._id)}> */}
+                  <span style={{color: 'red'}} onClick={() => openModal(user._id)}>
+                    Delete
                   </span>
                 </div>
               )
